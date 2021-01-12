ffd9b665c4bd7b3865c9e69ee108a13ac30cfc2b
Phuong <phuongpuna@gmail.com>
2021-01-12 22:11:41 +0700

    Add configuration to the app

---
+++ config.js
@@ -0,0 +1,15 @@
+//User could change these data.
+/**
+ * Customer could change these data such as images, avatars, texts
+ * In this example, customer should change the Title of the games, message on startup...
+ * And then, game developer could handle these data in html source code
+ * See index.html for more infomation.
+ * @title Title of the app
+ * @message Message to end-user
+ * @color App's color
+ */
+var data = {
+  title: "This is 2048",
+  message: "Hello world",
+  color: "#ad3931",
+};
