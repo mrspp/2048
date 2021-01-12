8ee772e9b26e0f0ab8280b8aa6f10958424d9b4e
Phuong <phuongpuna@gmail.com>
2021-01-12 22:31:36 +0700

    Update data structure

--- config.js
+++ config.js
@@ -9,7 +9,24 @@
  * @color App's color
  */
 var data = {
-  title: "This is 2048",
-  message: "Hello world",
-  color: "#ad3931",
+  title: {
+    name: "Title",
+    description: "Enter the game title here",
+    data: "This is 2048",
+  },
+  message: {
+    name: "Message",
+    description: "Message to the gamer",
+    data: "This is 2048",
+  },
+  color: {
+    name: "Color",
+    description: "Color for the app",
+    data: "#ad3931",
+  },
+  google_analytics_code: {
+    name: "Google Analytics",
+    description: "Enter Google analystic code here",
+    data: "UA-XXXX-XXX",
+  }, //analytics
 };
